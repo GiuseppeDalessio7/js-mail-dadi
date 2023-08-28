3c2370b2718576ecba629061bbf8ea11c81a6994
GiuseppeDalessio7 <g.dalessio03@gmail.com>
2023-08-28 17:31:41 +0200

    Push for help

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -8,35 +8,66 @@ Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un arra
 let randomPlayers = Math.floor(Math.random() * 6) + 1;
 console.log('Numero giocatore ' + randomPlayers);
 
-let randomPc = Math.floor(Math.random() * 6 ) + 1;
+let randomPc = Math.floor(Math.random() * 6) + 1;
 console.log('Numero pc ' + randomPc);
 
-let win 
+let win
 
 
-if (randomPlayers > randomPc ){
+if (randomPlayers > randomPc) {
     // alert('Ha vinto il giocatore');
     console.log('Ha vinto il giocatore');
     let win = ('Il giocatore')
-    let winner = document.getElementById('winner').innerHTML = ('Il vincitore è: ' + win)  
+    let winner = document.getElementById('winner').innerHTML = ('Il vincitore è: ' + win)
 
-    
 
-}if (randomPlayers < randomPc) {
+
+} if (randomPlayers < randomPc) {
     // alert('Ha vinto il pc');
     console.log('Ha vinto il pc');
     let win = ('Il Pc')
-    let winner = document.getElementById('winner').innerHTML = ('Il vincitore è: ' + win)  
+    let winner = document.getElementById('winner').innerHTML = ('Il vincitore è: ' + win)
 
 
-}else if (randomPlayers === randomPc){ 
+} else if (randomPlayers === randomPc) {
     // alert('Pareggio');
     console.log('Pareggio');
-    let win = ('Vi è stato')
-    let winner = document.getElementById('winner').innerHTML = ('Il vincitore è: ' + win)  
+    let win = ('Vi è stato un pareggio')
+    let winner = document.getElementById('winner').innerHTML = ('Il vincitore è: ' + win)
 
 }
-    
+
 let player = document.getElementById('player').innerHTML = ('Il numero del giocatore è: ' + randomPlayers)
 let pc = document.getElementById('pc').innerHTML = ('Il numero del pc è: ' + randomPc)
 
+
+
+
+/* Mail
+Chiedi all'utente la sua email, 
+controlla che sia nella lista di chi può accedere, 
+stampa un messaggio appropriato sull’esito del controllo.*/
+
+const emailUser = document.getElementById('email').value;
+const validator = document.querySelector("button")
+// console.log(emailUser);
+const emailList = ['srocco@gmail.com', 'maria2@gmail.com', 'giovanni3@gmail.com', 'carlo7@gmail.com']
+
+
+
+for (let i = 0; i < emailList.length; i++) {
+    const check = emailList[i];
+    console.log(check);
+
+    validator.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        if (check === emailList) {
+            console.log('sei dentro la lista');
+        } else {
+            console.log('sei fuori dalla lista');
+        }
+    });
+
+
+}
